af72175a8e0a66b738c083023c8f4e74acb8027b
alexbalak21 <alex.balak@outlook.com>
2022-01-08 22:30:02 +0100

    Need to connect to mango db

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 const articleRouter = require("./routes/articles");
+
+mongoose.connect("mongodb://localhost/blog", { useNewUrlParser: true });
 
 app.set("view engine", "ejs");
 
